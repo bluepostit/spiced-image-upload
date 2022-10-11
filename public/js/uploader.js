081e6029f10328e36eddfa39c4a6b10a6791b8ac
Yair <32864499+bluepostit@users.noreply.github.com>
2022-10-11 14:09:44 +0200

    finished version

--- public/js/uploader.js
+++ public/js/uploader.js
@@ -1,1 +1,39 @@
-// To Do
+const app = Vue.createApp({
+    data() {
+        return {
+            message: 'Please upload a file',
+            images: []
+        }
+    },
+    methods: {
+        upload(e) {
+            const form = e.currentTarget;
+            console.log({ form });
+
+            // get the file input
+            // check its files. 
+            // if no files, set error message!
+            const fileInput = form.querySelector('input[type=file]');
+            console.log(fileInput.files);
+
+            if (fileInput.files.length < 1) {
+                this.message = 'You must first select a file!';
+                return;
+            }
+
+            const myFormData = new FormData(form);
+
+            fetch(form.action, {
+                method: 'post',
+                body: myFormData
+            }).then(res => res.json())
+                .then((data) => {
+                    if (data.message) {
+                        this.message = data.message;
+                    }
+                    this.images.push(data.path);
+                })
+        }
+    }
+});
+app.mount('#main');
--- server.js
+++ server.js
@@ -2,9 +2,26 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
+const { uploader } = require('./middleware');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use(express.json());
+
+app.post('/image', uploader.single('file'), (req, res) => {
+    if (req.file) {
+        res.json({
+            success: true,
+            message: 'Thank you!',
+            path: `/uploads/${req.file.filename}`
+        });
+    } else {
+        res.json({
+            success: false,
+            message: 'Upload failed!'
+        });
+    }
+})
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
